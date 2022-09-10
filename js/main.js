da8f70a9a32971f3ba4a23fc89db6cb89aa79f5f
KRIENGKRAI YOTHEE <33215380+bornborn41@users.noreply.github.com>
2022-09-10 20:43:43 +0700

    cc

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 // Declare an async function
-const output = document.querySelector('#output');
-const url = "https://script.google.com/macros/s/AKfycbw1qI1o-xbDFGVNdo9rreIPRk3JoYNJCgDM9VvPG0JNgpiIsoFSvE4Ueq9VproYQjJnhQ/exec";
+const output = document.querySelector('.output');
+// const url = "https://script.google.com/macros/s/AKfycbw1qI1o-xbDFGVNdo9rreIPRk3JoYNJCgDM9VvPG0JNgpiIsoFSvE4Ueq9VproYQjJnhQ/exec";
 output.textContent = 'Context';
-document.addEventListener('DOMContentLoaded',init);
 
-function init(){
-    console.log('ready');
-}
+// document.addEventListener('DOMContentLoaded',init);
+
+// function init(){
+//     console.log('ready');
+// }
 
 // const getData = async () => {
 //   // Use the await keyword to let JS know this variable has some latency so it should wait for it to be filled 
