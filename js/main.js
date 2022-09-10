05fe483eac3e868f53d3e501c36041dac0b89811
KRIENGKRAI YOTHEE <33215380+bornborn41@users.noreply.github.com>
2022-09-10 20:36:31 +0700

    ready

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 // Declare an async function
 const url = "https://script.google.com/macros/s/AKfycbw1qI1o-xbDFGVNdo9rreIPRk3JoYNJCgDM9VvPG0JNgpiIsoFSvE4Ueq9VproYQjJnhQ/exec";
-const getData = async () => {
-  // Use the await keyword to let JS know this variable has some latency so it should wait for it to be filled 
-  // When the variable is fetched, use the .then() callback to carry on 
-    const DataJSON = await fetch(url).then(response => 
-        response.json()
-    )
+
+document.addEventListener('DOMContentLoaded',init);
+
+function init(){
+    console.log('ready');
+}
+
+// const getData = async () => {
+//   // Use the await keyword to let JS know this variable has some latency so it should wait for it to be filled 
+//   // When the variable is fetched, use the .then() callback to carry on 
+//     const DataJSON = await fetch(url).then(response => 
+//         response.json()
+//     )
     
-    return await DataJSON
-  };
+//     return await DataJSON
+//   };
