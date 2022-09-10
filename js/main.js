0452017ddf16d41310e39dd0967bb8fbabb3e165
KRIENGKRAI YOTHEE <33215380+bornborn41@users.noreply.github.com>
2022-09-10 20:28:05 +0700

    hh

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-async function populate() {
-
-    const requestURL = 'https://script.google.com/macros/s/AKfycbw1qI1o-xbDFGVNdo9rreIPRk3JoYNJCgDM9VvPG0JNgpiIsoFSvE4Ueq9VproYQjJnhQ/exec';
-    const request = new Request(requestURL);
-
-    const response = await fetch(request);
-    const superHeroes = await response.json();
-
-    populateHeader(superHeroes);
-    populateHeroes(superHeroes);
-    console(superHeroes);
-
-}
+// Declare an async function
+const url = "https://script.google.com/macros/s/AKfycbw1qI1o-xbDFGVNdo9rreIPRk3JoYNJCgDM9VvPG0JNgpiIsoFSvE4Ueq9VproYQjJnhQ/exec";
+const getData = async () => {
+  // Use the await keyword to let JS know this variable has some latency so it should wait for it to be filled 
+  // When the variable is fetched, use the .then() callback to carry on 
+    const DataJSON = await fetch(url).then(response => 
+        response.json()
+    )
+    
+    return await DataJSON
+  };
